8b1b6344bcdd60c0b2c7299866a5f01dca58f4b2
Matthew Cles <matiu1.00@hotmail.com>
2019-02-04 22:22:33 -0800

    Add download location to config file

--- get_wallpapers.py
+++ get_wallpapers.py
@@ -13,6 +13,11 @@ config_file.close()
 
 current_number_of_photos = 0
 
+# Create picture directory if it does not exist
+if not os.path.exists(config['download_location']):
+  print('Createing directory: ' + config['download_location'])
+  os.makedirs(config['download_location'])
+
 while current_number_of_photos < config['min_number_of_photos']:
 
   # Chose the collection
@@ -21,21 +26,19 @@ while current_number_of_photos < config['min_number_of_photos']:
 
   # Write to log file
   now = datetime.datetime.now()
-  f = open('./collection_key_log.txt', 'a')
+  f = open(config['download_location'] + '/collection_key_log.txt', 'a')
   f.write(now.strftime('%m/%d/%Y @ %H:%M') + ': Collection "' + collection_name + '" selected' + '\n')
   f.close()
   
-  # Create picture directory if it does not exist
-  if not os.path.exists('./pics'):
-    os.makedirs('./pics')
+  
 
   # Replace old photos with photos from the new collection
   for x in range(config['max_number_of_photos']):
-    if os.path.exists('./pics/pic' + str((x + 1)) + '.jpeg'):
-      os.unlink('./pics/pic' + str((x + 1)) + '.jpeg')
+    if os.path.exists(config['download_location'] + '/pic' + str((x + 1)) + '.jpeg'):
+      os.unlink(config['download_location'] + '/pic' + str((x + 1)) + '.jpeg')
 
     url = 'https://source.unsplash.com/collection/' + str(collection_value) + '/' + config['resolution'] + '.jpeg'
-    filename = wget.download(url, out = "./pics/pic" + str((x + 1)) + ".jpeg")
+    filename = wget.download(url, out = config['download_location'] + '/pic' + str((x + 1)) + '.jpeg')
     current_number_of_photos = current_number_of_photos + 1
     # Wait 3 seconds between getting new photos to prevent downloading cashed duplicates
     time.sleep(3)
@@ -44,8 +47,8 @@ while current_number_of_photos < config['min_number_of_photos']:
   # Compare size of pictures to prevent duplicates. 
   sizeCheckList = []
   for x in range(config['max_number_of_photos']):
-    size = os.path.getsize('./pics/pic' + str((x + 1)) + '.jpeg')
+    size = os.path.getsize(config['download_location'] + '/pic' + str((x + 1)) + '.jpeg')
     if size in sizeCheckList:
-      os.unlink('./pics/pic' + str((x + 1)) + '.jpeg')
+      os.unlink(config['download_location'] + '/pic' + str((x + 1)) + '.jpeg')
       current_number_of_photos = current_number_of_photos - 1
     sizeCheckList.append(size)
